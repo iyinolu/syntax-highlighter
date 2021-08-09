55990eea6e17d2adaad47e195d00e388e14967f7
Paul Osinowo <47259302+iyinolu@users.noreply.github.com>
2021-08-09 05:00:28 +0100

    Feature: add basic authentication to create endpoint

--- syntaxhighlighter/views.py
+++ syntaxhighlighter/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
-from rest_framework import generics, serializers
+from rest_framework import permissions, renderers
+from rest_framework.generics import *
+from rest_framework.response import Response
 from .serializers import HighlightedTextSerializer
 from .models import Snippets
-# Create your views here.
+from .permissions import IsOwnerOrReadOnly
 
-class SnippetList(generics.ListAPIView):
+
+class SnippetList(ListAPIView):
     queryset = Snippets.objects.all()
     serializer_class = HighlightedTextSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly, 
+        IsOwnerOrReadOnly
+    ]
 
-class GetSnippet(generics.RetrieveAPIView):
+class SnippetCreateRetrieve(CreateAPIView, RetrieveAPIView, GenericAPIView):
+
     queryset = Snippets.objects.all()
     serializer_class = HighlightedTextSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class SnippetHighlight(GenericAPIView):
+    queryset = Snippets.objects.all()
+    renderers_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+    
